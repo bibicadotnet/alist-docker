e6643107b7956506efb9a1f424da2ec24521824e
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-08-22 14:32:05 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+sudo apt update -y
+
+locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+sudo apt remove iptables-persistent -y
+sudo ufw disable
+sudo iptables -F
+
+timedatectl set-timezone Asia/Ho_Chi_Minh
+sudo fallocate -l 4G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker $(whoami)
+sudo systemctl start docker
+sudo systemctl enable docker
+apt install docker-compose -y
+
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/NeverIdle-Oracle/master/VM.Standard.E2.1.Micro.sh -O /usr/local/bin/bypass_oracle.sh
+chmod +x /usr/local/bin/bypass_oracle.sh
+nohup /usr/local/bin/bypass_oracle.sh >> ./out 2>&1 <&- &
+crontab -l > bypass_oracle
+echo "@reboot nohup /usr/local/bin/bypass_oracle.sh >> ./out 2>&1 <&- &" >> bypass_oracle
+crontab bypass_oracle
